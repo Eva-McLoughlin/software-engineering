2c69199e426166f1abf7710a8397cc2838c3e4f2
Eva-McLoughlin <mclougev@tcd.ie>
2018-10-16 16:33:26 +0100

    DAG class - initial code
    needs work and also want to add some explanatory comments

--- BST_LCA/src/DAG.java
+++ BST_LCA/src/DAG.java
@@ -1,4 +1,14 @@
 
-public class DAG {
+import java.util.Arrays;
 
+public class DAG <Value> {
+	private class Node {
+		private Value val;	
+		private Node[] successors;		
+	
+		public Node(Value val) {
+			this.val = val;
+		}
+	}
+	private Node[] nodeList = new DAG.Node[0];
 }
