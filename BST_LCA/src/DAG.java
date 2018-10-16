baef7ecee7f5315e4cda41c3f1e54104c018c634
Eva-McLoughlin <mclougev@tcd.ie>
2018-10-16 16:52:31 +0100

    Added method to check if DAG is empty

--- BST_LCA/src/DAG.java
+++ BST_LCA/src/DAG.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class DAG <Value> {
 	private class Node {
-		private Value val;	
+		private Value val;	// This is the value stored in the node
 		private Node[] successors;		
 	
 		public Node(Value val) {
@@ -11,4 +11,18 @@ public class DAG <Value> {
 		}
 	}
 	private Node[] nodeList = new DAG.Node[0];
+	
+	public boolean isEmpty() { //method to check if the DAG is empty
+		if (size()==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private int size() {
+		// TODO Auto-generated method stub
+		return nodeList.length;  //returns the number of nodes
+	}
 }
